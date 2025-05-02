be2ef1461ddd6d57c291726fd5bc8f67a8793a65
João M. M. Leal <j.marcos.mla@gmail.com>
2025-05-02 12:00:53 -0300

    Atividade Java ( while / for )

---
+++ ativi5.java
@@ -0,0 +1,10 @@
+public class ativi5 {
+    public static void main(String[] args) {
+        long fatorial = 1;
+
+        for (long i = 1; i <= 20; i++) {
+            fatorial *= i;
+            System.out.println(fatorial);
+        }
+    }
+}
---
+++ ativi6.java
@@ -0,0 +1,15 @@
+public class ativi6 {
+     public static void main(String[] args) {
+            int a = 0;
+            int b = 1;
+    
+            System.out.println("Série de Fibonacci até passar de 100:");
+    
+            while (a <= 100) {
+                System.out.print(a + " ");
+                int proximo = a + b;
+                a = b;
+                b = proximo;
+            }
+    } 
+}
